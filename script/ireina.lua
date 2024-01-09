2b612536a1e20d5ab95335d2e3c06f4b452db7c4
slyni <slyni@naver.com>
2024-01-09 22:41:09 +0900

    Update ireina.lua

--- script/ireina.lua
+++ script/ireina.lua
@@ -3381,7 +3381,7 @@ function Card.RegisterEffect(c,e,forced,...)
 				end
 			end
 			if cost then
-				cost(e,tp,eg,ep,ev,re,r,rp,0)
+				cost(e,tp,eg,ep,ev,re,r,rp,1)
 			end
 		end)
 		if tg then
